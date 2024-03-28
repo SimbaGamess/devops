78c8f4132c5fa7776ffdaeb9273c93fabbfe1d5d
SimbaGamess <115343546+SimbaGamess@users.noreply.github.com>
2024-03-28 09:47:47 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,7 +17,7 @@ private String buildString(){
 }
   
   private void outPrint(String printing){
-    for (int i = 0; i < printing.length; i++){
+    for (int i = 0; i < printing.length(); i++){
       System.out.println(printing.charAt(i));
     }
   }
